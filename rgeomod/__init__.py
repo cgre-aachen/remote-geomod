e847ef8f62c1df1212d09bfd5d900c0d767690e8
Alex <alexander.schaaf@rwth-aachen.de>
2018-01-11 14:23:26 +0100

    - geotiff export function

--- rgeomod/__init__.py
+++ rgeomod/__init__.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 from time import sleep
 from matplotlib.patches import FancyArrowPatch
 from mpl_toolkits.mplot3d import proj3d
+import gdal
 
 # TODO: we should rework the storage of xyz coordinates from single .x, .y, .z class-variables to np.ndarray
 
@@ -254,6 +255,53 @@ def convert_to_df(ks, ks_names, filenames, ks_bool):
     # TODO: flexible formation number assignment for foliations
 
     return interfaces, foliations
+
+
+def export_geotiff(path, geo_map, cmap, geotiff_filepath):
+    """
+
+    Args:
+        path (str): Filepath for the exported geotiff, must end in .tif
+        geo_map (np.ndarray): 2-D array containing the geological map
+        cmap (matplotlib colormap): The colormap to be used for the export
+        geotiff_filepath (str): Filepath of the template geotiff
+
+    Returns:
+        Saves the geological map as a geotiff to the given path.
+    """
+
+    # **********************************************************************
+    geo_map_rgb = cmap(geo_map)  # r,g,b,alpha
+    # **********************************************************************
+    # gdal.UseExceptions()
+
+    ds = gdal.Open(geotiff_filepath)
+    band = ds.GetRasterBand(1)
+    arr = band.ReadAsArray()
+    [cols, rows] = arr.shape
+
+    outFileName = path
+    driver = gdal.GetDriverByName("GTiff")
+    options = ['PROFILE=GeoTiff', 'PHOTOMETRIC=RGB', 'COMPRESS=JPEG']
+    outdata = driver.Create(outFileName, rows, cols, 3, gdal.GDT_Byte, options=options)
+
+    outdata.SetGeoTransform(ds.GetGeoTransform())  # sets same geotransform as input
+    outdata.SetProjection(ds.GetProjection())  # sets same projection as input
+    outdata.GetRasterBand(1).WriteArray(geo_map_rgb[:, ::-1, 0].T * 256)
+    outdata.GetRasterBand(2).WriteArray(geo_map_rgb[:, ::-1, 1].T * 256)
+    outdata.GetRasterBand(3).WriteArray(geo_map_rgb[:, ::-1, 2].T * 256)
+    outdata.GetRasterBand(1).SetColorInterpretation(gdal.GCI_RedBand)
+    outdata.GetRasterBand(2).SetColorInterpretation(gdal.GCI_GreenBand)
+    outdata.GetRasterBand(3).SetColorInterpretation(gdal.GCI_BlueBand)
+    # outdata.GetRasterBand(4).SetColorInterpretation(gdal.GCI_AlphaBand)  # alpha band
+
+    # outdata.GetRasterBand(1).SetNoDataValue(999)##if you want these values transparent
+    outdata.FlushCache()  # saves to disk
+    outdata = None  # closes file (important)
+    band = None
+    ds = None
+
+    print("Successfully exported geological map to "+path)
 
 
 def calculate_gradient(foliations):
