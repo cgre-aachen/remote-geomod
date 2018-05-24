80465b2ddf87d945b0e048b0c00f7ce8b498335c
Miguel de la Varga <varga@aices.rwth-aachen.de>
2018-05-24 13:35:34 +0200

    Revert
    "Hotfix: - [Installation] Added setup.py"
    
    This reverts commit 3a6c2a9
    
    - Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='remote-geomod',
+    version='',
+    packages=['build.lib.rgeomod', 'rgeomod'],
+    url='',
+    license='Lesser GNU',
+    author='Alexander Shaaf, Florian Wellmann, Miguel de la Varga',
+    author_email='wellmann@aices.rwth-aachen.de',
+    description=''
+)
